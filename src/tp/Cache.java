f3ae28601f0057c3654b3b7b0b409a89c8b35f4f
MayconFerreira <goulartduda97@gmail.com>
2017-12-01 17:16:01 -0200

    Classes comentadas

--- src/tp/Cache.java
+++ src/tp/Cache.java
@@ -5,18 +5,20 @@ package tp;
  */
 public class Cache {
 
-    private final Conjunto[] conjuntos;
-    private final Politica politica;
-    private final int BLOCKSIZE;
+    private final Conjunto[] conjuntos; //vetor de Conjuntos que serão adicionados na cache
+    private final Politica politica; //política de substituição que será utilizada
+    private final int BLOCKSIZE; //tamanho do bloco
+    
     
     protected Cache(int CACHESIZE, int numVias, int numBlocos, Politica politicaDeSubstituicao){
 
-        conjuntos = new Conjunto[numVias];
-        BLOCKSIZE = CACHESIZE / numBlocos;
-        politica = politicaDeSubstituicao;
+        conjuntos = new Conjunto[numVias]; //cada conjunto possui como tamanho a quantidade de vias
+        BLOCKSIZE = CACHESIZE / numBlocos; //tamanho do bloco é o tamanho ca cache dividido pela quantidade de blocos
+        politica = politicaDeSubstituicao; //recebe a política de substituição escolhida
 
-        for(int i = 0; i < conjuntos.length; i++){
-
+        for(int i = 0; i < conjuntos.length; i++){ 
+        	/*Preenche cada posição do conjunto com a quantidade de blocos por conjunto, 
+        	 * dado pela quantidade de blocos dividida pelo número de vias*/
             conjuntos[i] = new Conjunto(numBlocos / numVias);
         }
     }
@@ -30,17 +32,24 @@ public class Cache {
         
         return conjuntos[index];
     }
-
+    
+    /*cada índice determina a posição que a palavra ocupa no bloco, e é calculado
+     * pela divisão do valor do endereço pelo tamanho do bloco, mod o tamanho do conjunto*/
     protected int calcularIndex(int endereco){
 
         return (int) ((endereco / BLOCKSIZE) % conjuntos.length);
     }
-
+    
+    /*para calcular os bits da tag basta dividir o endereço pelo tamanho do bloco e em seguida pelo tamanho do conjunto*/
     protected int calcularTag(int endereco){
 
         return endereco / BLOCKSIZE / conjuntos.length;
     }
 
+    /*método que irá buscar se o endereço está localizado naquele bloco,
+     * para isso em cada bloco irá buscar pelo índice daquele endereço,
+     * caso estiver lá, então retorna o bloco onde foi encontrado. Caso contrário,
+     * retorna nulo mostrando que não foi possível encontrar o endereço*/
     protected Bloco buscarBloco(int endereco){
 
         int tag = calcularTag(endereco);
@@ -55,7 +64,10 @@ public class Cache {
         }
         return null;
     }
-
+    
+    /*Busca se existe o endereço no bloco, utilizando o método buscarBloco, caso exista
+     * confere se o estado não é inválido, caso não seja o bloco é válido. Se o endereço não estiver
+     * em um bloco ou o estado for inávlido, então aquele bloco é inválido*/
     protected boolean hasBlocoValido(int endereco){
 
         Bloco bloco = buscarBloco(endereco);
@@ -70,6 +82,7 @@ public class Cache {
         return false;
     }
 
+    /**/
     protected boolean writeHit(int endereco){
         
         Bloco bloco = buscarBloco(endereco);
